4137e6c95923b923ea7787daf35025eeec32d7d7
Shayne Philibert <sphilibert@gmail.com>
2023-01-02 10:32:21 -0500

    first working copy of web scraper, that scrapes graal depot body file

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import requests
+import re
+
+
+def download_pngs(url, directory):
+    # Send an HTTP request to the URL of the webpage you want to access.
+    response = requests.get(url)
+
+    # Check if the response is valid.
+    if response.status_code == 200:
+        # Get the webpage's content and store it in a variable.
+        html = response.text
+    else:
+        print("Invalid response.")
+        return
+
+    # Find all of the png image URLs on the webpage using a regular expression.
+    png_urls = re.findall('https://.+?\.png', html)
+
+    # Iterate over the list of png URLs.
+    for url in png_urls:
+        # Get the filename by splitting the URL and getting the last element.
+        filename = url.split("/")[-1]
+
+        # Create the full path by joining the directory and the filename.
+        path = os.path.join(directory, filename)
+
+        # Download the image data and save it to a file.
+        with open(path, "wb") as f:
+            f.write(requests.get(url).content)
+
+
+# Extracts the page number from a url after the "=" sign
+def getcurrentpage(url):
+    url = url.split('=')
+    return url[1]
+
+
+# Extracts all text in a url before the "=" sign
+def spliturl(url):
+    url = url.split('=')
+    return url[0] + "="
+
+
+#
+def enumeratepage(url):
+    urlname = spliturl(url)
+    urlnum = int(getcurrentpage(url)) + 1
+    return urlname + str(urlnum)
+
+
+def iteratepage(url):
+    # Asks the user to specify the total number of pages that will be scanned
+    totalpagenumber = int(input("Enter the total number of web pages to enumerate"))
+    for i in range(totalpagenumber):
+        download_pngs(url, "images")
+        url = enumeratepage(url)
+
+
+iteratepage("https://www.graaldepot.com/graal-bodies?nggpage=1")
+
